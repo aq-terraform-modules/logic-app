a7ad28531dc7e7d9daa433867c3f51206ab2c351
Anh Quach <anh.quachquituan@halliburton.com>
2021-04-12 17:30:42 +0700

    Add template deployment

--- main.tf
+++ main.tf
@@ -23,3 +23,56 @@ resource "azurerm_logic_app_trigger_recurrence" "logicapptrigger" {
   frequency    = "Day"
   interval     = 1
 }
+
+resource "azurerm_resource_group_template_deployment" "example" {
+  name                = "${var.logic_app_name}-sqlconnection"
+  resource_group_name = azurerm_resource_group.logicapprg.name
+  deployment_mode     = "Incremental"
+  parameters_content = jsonencode({
+    "vnetName" = {
+      value = local.vnet_name
+    }
+  })
+  template_content = <<TEMPLATE
+{
+    "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "connections_sql_name": {
+            "defaultValue": "sql",
+            "type": "String"
+        },
+        "location": {
+            "defaultValue": "southeastasia",
+            "type": "String"
+        }
+    },
+    "variables": {},
+    "resources": [
+        {
+            "type": "Microsoft.Web/connections",
+            "apiVersion": "2016-06-01",
+            "name": "[parameters('connections_sql_name')]",
+            "location": "[parameters('location')]",
+            "properties": {
+                "displayName": "Test",
+                "customParameterValues": {},
+                "api": {
+                    "id": "[concat('/subscriptions/7bd03bca-6ad8-4e23-87e7-88457e614216/providers/Microsoft.Web/locations/', parameters('location'), '/managedApis/', parameters('connections_sql_name'))]"
+                },
+                "parameterValues": {
+                  "server": "msql-vidm.database.windows.net",
+                  "database": "LMK_Deployment",
+                  "username": "haladmin",
+                  "password": "haladmin",
+                   "authType": "basic",
+                }
+            }
+        }
+    ]
+}
+TEMPLATE
+
+  // NOTE: whilst we show an inline template here, we recommend
+  // sourcing this from a file for readability/editor support
+}
